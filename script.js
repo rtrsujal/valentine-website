2bd06a386f75a4aa30d2fc19894ff3ff8e355e7e
rtrsujal <rtr.sujalrcma@gmail.com>
2026-02-01 13:31:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ yesBtn.addEventListener("click", () => {
         </div>
     `;
 });
+setInterval(() => {
+    const heart = document.createElement("div");
+    heart.innerHTML = "💖";
+    heart.style.position = "fixed";
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.top = "100vh";
+    heart.style.fontSize = "24px";
+    heart.style.animation = "floatUp 4s linear";
+    document.body.appendChild(heart);
+
+    setTimeout(() => heart.remove(), 4000);
+}, 500);

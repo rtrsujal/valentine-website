4235239a3f687ee21dd64ed12e0525884aa06a32
rtrsujal <rtr.sujalrcma@gmail.com>
2026-02-01 14:48:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const yesBtn = document.getElementById("yesBtn");
 const noBtn = document.getElementById("noBtn");
 const message = document.getElementById("message");
+const yesGif = document.getElementById("yesGif");
 
 yesBtn.addEventListener("click", () => {
-  message.innerHTML = "YAYYY 💖 I knew you'd say yes 😍💫";
+  message.innerHTML = "YAYYY 💖 I knew you'd say yes 😍";
+  yesGif.style.display = "block";
 });
 
 noBtn.addEventListener("mouseover", () => {
